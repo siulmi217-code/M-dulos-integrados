64d595382f56f9b0eff72e3eb519ff4cf3fd6472
Luis Mendez <siulmi217@gmail.com>
2026-05-01 12:15:27 -0500

    Subida del frontend

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
-import { NgModule } from '@angular/core';
+ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {FormsModule} from '@angular/forms'; 
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmpleadosComponent } from './components/empleados/empleados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmpleadosComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/empleados/empleados.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { EmpleadoService } from "../../services/empleado.service";
+import { NgForm } from '@angular/forms';
+import { Empleado } from 'src/app/models/empleado';
+ 
+declare var M: any;
+@Component({
+  selector: 'app-empleados',
+  templateUrl: './empleados.component.html',
+  styleUrls: ['./empleados.component.css'],
+  providers: [EmpleadoService]
+})
+export class EmpleadosComponent implements OnInit {
+ 
+  constructor(public empleadoService: EmpleadoService) { }
+  ngOnInit(): void {
+  }
+  agregarEmpleado(form?: NgForm) {
+    this.empleadoService.PostEmpleado(form?.value)
+      .subscribe(res => {
+        this.resetForm(form);
+        M.toast({html: 'Guardado satisfactoriamente'});
+      });
+  }
+  resetForm(form?: NgForm) { // Limpiar el formulario, recibe un formulario como parametro
+    if (form) {
+      form.reset();
+      this.empleadoService.selectedEmpleado = new Empleado();
+    }
+  }
+}
